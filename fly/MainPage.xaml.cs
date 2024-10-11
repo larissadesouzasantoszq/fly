c5953f75f5a5ecc110ca98c97dc2e005e3b11461
larissa <larissadesouzadossantossouzq@gmail.com>
2024-10-11 08:55:47 -0300

    comeco

---
+++ fly/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace fly;
+
+public partial class MainPage : ContentPage
+{
+
+	public MainPage()
+	{
+		InitializeComponent();
+	}
+
+	
+}
+
---
+++ fly/Platforms/MacCatalyst/AppDelegate.cs
@@ -0,0 +1,9 @@
+﻿using Foundation;
+
+namespace fly;
+
+[Register("AppDelegate")]
+public class AppDelegate : MauiUIApplicationDelegate
+{
+	protected override MauiApp CreateMauiApp() => MauiProgram.CreateMauiApp();
+}
