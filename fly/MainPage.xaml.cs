df554ce89b175440267f736e317f0f64ec5476f7
larissa <larissadesouzadossantossouzq@gmail.com>
2024-10-15 08:54:10 -0300

    continuando o csS

--- fly/MainPage.xaml.cs
+++ fly/MainPage.xaml.cs
@@ -2,12 +2,41 @@
 
 public partial class MainPage : ContentPage
 {
-
+	const int TempoEntreFrames = 25;
+	const int gravidade = 3;
+	double LarguraJanela;
+	double AlturaJanela;
+	int velocidade = 20;
+	bool estaMorto = true;
 	public MainPage()
 	{
 		InitializeComponent();
 	}
+	public async void Desenha()
+	{
+		while (!estaMorto)
+		{
+			AplicaGravidade();
+			GerenciaCanos();
+			await Task.Delay(TempoEntreFrames);
+		}
+	}
+	void AplicaGravidade()
+	{
+		Passaro.TranslationY += Gravidade:
+		}
 
-	
+	protected override void OnSizeAllocated(double w, double h)
+	{
+		base.OnSizeAllocated(w, h);
+		LarguraJanela = w;
+		AlturaJanela = h;
+
+	}
+	void GerenciaCanos()
+	{
+		imgCanoCima.TransltionX -= velocidade;
+		imgCanoBaixo.TransltionX -= velocidade;
+
+		if ()
 }
-
